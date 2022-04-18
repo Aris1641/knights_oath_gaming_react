5cc65ae5f11befca67df9bb344f935818b3c0865
Aris Ceballos <aris1641@yahoo.com>
2022-04-17 21:04:40 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from './Containers/Home';
+import About from './Containers/About';
+import Founders from './Containers/Founders';
+import { Switch, Route, Link } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/founders">Founders</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/founders" component={Founders} />
+
+      </Switch>
     </div>
+
   );
 }
 
 export default App;
+
---
+++ src/Containers/Founders.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Founders() {
+    return <h2>Founders</h2>;
+}
+
+export default Founders;
